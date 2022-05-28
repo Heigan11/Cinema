ed6cd1062ccd86f76271f689660878095f102b08
Heigan <heigan11@gmail.com>
2022-05-28 22:50:24 +0300

    some fix before evo, need fix user id in signin

--- ex02/Cinema/src/main/resources/sql/schema.sql
+++ ex02/Cinema/src/main/resources/sql/schema.sql
@@ -1,8 +1,4 @@
-drop table if exists halls;
-CREATE TABLE halls(
-                      id SERIAL PRIMARY KEY,
-                      seats integer not null);
-drop table if exists movies;
+drop table if exists movies cascade;
 CREATE TABLE movies(
                       id SERIAL PRIMARY KEY,
                       title varchar(50) not null,
@@ -10,3 +6,7 @@ CREATE TABLE movies(
                       restriction integer not null,
                       description varchar(1000) not null,
                       poster varchar(1000));
+drop table if exists halls cascade;
+CREATE TABLE halls(
+                      id SERIAL PRIMARY KEY,
+                      seats integer not null);
